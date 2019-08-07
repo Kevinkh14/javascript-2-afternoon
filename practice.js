eca62f9f555fe49c7ef421e02c708ffe7ecd2c78
kevin <kevin.hernandez1802@gmail.com>
2019-08-07 14:10:49 -0500

    finished

--- practice.js
+++ practice.js
@@ -15,7 +15,11 @@
 */
 
 //Code here
-
+var me = {
+  name: "kevin",
+  age : 20
+}
+me.name;
 ////////// PROBLEM 2 //////////
 
 /*
@@ -24,21 +28,30 @@
 */
 
 //Code here
-
+var favoriteThings = {
+  band: "None",
+  food: "chicken sandwitches",
+  person:"Mom",
+  book:"dont read",
+  movie:"the fast and the furious",
+  holiday: "Christmas"
+}
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
 //Code here
-
+favoriteThings.car = "Toyota Supra"
+favoriteThings.brand = "Addidas"
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
-
+favoriteThings.food = "Chicken Nuggets"
+favoriteThings.book = 'Harry Potter'
 ////////// PROBLEM 3 //////////
 
 /*
@@ -50,11 +63,14 @@
 */
 
 //Code here
+var backPack = {}
+var item = 'firstPocket'
+backPack[item] ='chapstick'
 
 /*
   After you do the above, alert your entire backPack object.
 */
-
+//alert(backPack)
 //Code here
 
 /*
@@ -64,7 +80,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
-
+console.log(backPack)
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -85,7 +101,8 @@ var user2 = {
 */
 
 //Code Here
-
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
@@ -96,7 +113,8 @@ var user2 = {
 */
 
 //Code Here
-
+let methodCollection = {
+}
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
